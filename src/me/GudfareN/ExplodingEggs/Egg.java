59446362a66f7181bdc3ca32dba1d5eaeb469fa9
Stian Tofte <stitofte@gmail.com>
2012-01-17 05:02:26 +0100

    If explosion is disabled - Then chickens will spawn again.

--- src/me/GudfareN/ExplodingEggs/Egg.java
+++ src/me/GudfareN/ExplodingEggs/Egg.java
@@ -3,6 +3,7 @@ package me.GudfareN.ExplodingEggs;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.event.entity.EntityListener;
+import me.GudfareN.ExplodingEggs.Explode;
 
 
 public class Egg extends EntityListener {
@@ -17,8 +18,14 @@ public class Egg extends EntityListener {
     
     public void onCreatureSpawn(CreatureSpawnEvent event)
     {
-        if (event.getSpawnReason() == SpawnReason.EGG) {
-            event.setCancelled(true);      
+    	if (Explode.ee.containsValue(true)){
+            if (event.getSpawnReason() == SpawnReason.EGG) {
+                event.setCancelled(true);
+    	}else{
+            if (event.getSpawnReason() == SpawnReason.EGG) {
+                event.setCancelled(false);
+    }     
+    }
+   }
   }
- }
 }
